0c415cd498d8381145b3bc00b827bae5fadf3750
Zac <a19860101@gmail.com>
2026-05-28 20:40:32 +0800

    20260528

--- 001.py
+++ 001.py
@@ -1,12 +1,12 @@
-import sys
-
 import cv2
 import sys
 
 img = cv2.imread('cat.jpg')
 
 if img is None:
+    print('picture is none')
     sys.exit()
+
 print(img)
 print(img.shape)
 print(type(img))
---
+++ 002.py
@@ -0,0 +1,17 @@
+import numpy as np
+
+# n = np.array([1,2,3])
+# n2 = np.array([[1,2,3],[4,5,6]])
+# n3 = np.array([
+#     [
+#         [1,2,3],
+#         [4,5,6],
+#         [7,8,9]
+#     ]
+# ])
+# print(n3)
+
+canvas = np.zeros((800, 600, 3), dtype=np.uint8)
+# print(canvas)
+canvas2 = np.ones((800, 600, 3), dtype=np.uint8)
+print(canvas2)
