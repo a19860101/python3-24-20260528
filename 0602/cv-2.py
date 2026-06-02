62b7f04f8f8e98dd370739e414bf2b8e704a33ae
Zac <a19860101@gmail.com>
2026-06-02 19:46:34 +0800

    20260602

--- 0602/cv-2.py
+++ 0602/cv-2.py
@@ -7,9 +7,11 @@ img3 = cv2.rotate(img, cv2.ROTATE_90_CLOCKWISE)
 img4 = cv2.rotate(img, cv2.ROTATE_90_COUNTERCLOCKWISE)
 
 img_flip = cv2.flip(img, 1)
+# 1 ：水平翻轉 (左右翻轉)
+# 0：垂直翻轉 (上下翻轉)
+# -1 ：水平＋垂直翻轉
 
 cv2.imshow('cat', img_flip)
-# cv2.imshow('cat', img3)
-# cv2.imshow('cat', img4)
+
 cv2.waitKey(0)
 cv2.destroyAllWindows()
