3934abbba423e29e5de17f47b5bf71b0213bbdf9
Zac <a19860101@gmail.com>
2026-06-02 21:08:45 +0800

    20260602

--- 0602/cv-face-1.py
+++ 0602/cv-face-1.py
@@ -7,6 +7,14 @@ face_cascades = cv2.CascadeClassifier(cv2.data.haarcascades + 'haarcascade_front
 img = cv2.imread("../images/man.png")
 img_gray = cv2.cvtColor(img, cv2.COLOR_BGR2GRAY)
 
-faces = face_cascades.detectMultiScale(img_gray, 1.3, 5)
+faces = face_cascades.detectMultiScale(img_gray, 1.2, 8,minSize=(100,100))
 
-print(faces)
+# print(faces)
+
+for (x,y,w,h) in faces:
+    print(x,y,w,h)
+    cv2.rectangle(img,(x,y),(x+w,y+h),(0,255,0),2)
+
+cv2.imshow("img", img)
+cv2.waitKey(0)
+cv2.destroyAllWindows()
