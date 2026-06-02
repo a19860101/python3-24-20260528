f7a050512b13d0af8eb6db382c7442d1011e388c
Zac <a19860101@gmail.com>
2026-06-02 21:38:10 +0800

    20260602

--- 0602/cv-face-1.py
+++ 0602/cv-face-1.py
@@ -2,8 +2,8 @@ import cv2
 
 # 載入模型
 # face_cascades = cv2.CascadeClassifier('haarcascade_frontalface_default.xml')
-# face_cascades = cv2.CascadeClassifier(cv2.data.haarcascades + 'haarcascade_frontalface_default.xml')
-face_cascades = cv2.CascadeClassifier(cv2.data.haarcascades + 'haarcascade_eye.xml')
+face_cascades = cv2.CascadeClassifier(cv2.data.haarcascades + 'haarcascade_frontalface_default.xml')
+# eye_cascades = cv2.CascadeClassifier(cv2.data.haarcascades + 'haarcascade_eye.xml')
 
 img = cv2.imread("../images/man.png")
 img_gray = cv2.cvtColor(img, cv2.COLOR_BGR2GRAY)
