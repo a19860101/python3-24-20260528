d3c69e4becd1692a751eb3ddcdc0b7b6418993b2
Zac <a19860101@gmail.com>
2026-06-02 19:11:41 +0800

    20260602

--- 0602/cv-1.py
+++ 0602/cv-1.py
@@ -17,6 +17,11 @@ img = cv2.imread('../ images/cat.jpg', cv2.IMREAD_REDUCED_GRAYSCALE_4)
 
 cv2.imshow('cat', img)
 
+cv2.imwrite('cat_gray.jpg', img)
+cv2.imwrite('cat_gray.png', img)
+cv2.imwrite('cat_gray.webp', img)
+
+
 cv2.waitKey(0)
 # 0表示程式會停在這裡，直到按下鍵盤上的任意鍵，才會繼續執行下一行。
 # 100則為100毫秒，表示視窗會停留100毫秒（0.1秒）
