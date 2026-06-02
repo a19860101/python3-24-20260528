bc9d1e733522bc4a895696531f62ed3450e2eed7
Zac <a19860101@gmail.com>
2026-06-02 19:03:01 +0800

    20260602

--- 0602/cv-1.py
+++ 0602/cv-1.py
@@ -13,6 +13,12 @@ img = cv2.imread('../ images/cat.jpg', cv2.IMREAD_REDUCED_GRAYSCALE_4)
 # cv2.IMREAD_REDUCED_GRAYSCALE_2：讀取並轉灰階，長寬變為原本的 1/2。
 # cv2.IMREAD_REDUCED_GRAYSCALE_4：讀取並轉灰階，長寬變為原本的 1/4。
 # cv2.IMREAD_REDUCED_GRAYSCALE_8：讀取並轉灰階，長寬變為原本的 1/8。
+
+
 cv2.imshow('cat', img)
+
 cv2.waitKey(0)
+# 0表示程式會停在這裡，直到按下鍵盤上的任意鍵，才會繼續執行下一行。
+# 100則為100毫秒，表示視窗會停留100毫秒（0.1秒）
+
 cv2.destroyAllWindows()
