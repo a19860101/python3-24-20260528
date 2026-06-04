09583d7be32cf3fedef9438f0561e968a2c3af5b
Zac <a19860101@gmail.com>
2026-06-04 19:46:51 +0800

    20260604

--- 0604/cv-video-3.py
+++ 0604/cv-video-3.py
@@ -1,4 +1,5 @@
 import cv2
+import numpy as np
 
 video_source = '../video.mp4'
 cap = cv2.VideoCapture(0)
@@ -6,11 +7,12 @@ cap = cv2.VideoCapture(0)
 while True:
     ret, frame = cap.read()
 
+    bg = np.zeros(frame.shape, np.uint8)
     small_frame = cv2.resize(frame, (0,0), fx=0.5, fy=0.5)
-    flip_frame = cv2.flip(small_frame, 1)
-    rotate_frame = cv2.rotate(frame, cv2.ROTATE_90_CLOCKWISE)
 
-    cv2.imshow('frame', frame)
+    bg[:240, :320] = small_frame
+
+    cv2.imshow('frame', bg)
 
     if cv2.waitKey(1) == ord('q'):
         break
