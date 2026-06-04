259c1148a387f43f4005253d11905c1eead496b8
Zac <a19860101@gmail.com>
2026-06-04 20:52:08 +0800

    20260604

--- 0604/cv-video-4.py
+++ 0604/cv-video-4.py
@@ -7,8 +7,11 @@ cap = cv2.VideoCapture(0)
 while True:
     ret, frame = cap.read()
 
+    # 邊緣檢測
+    # frame = cv2.Canny(frame,100,10)
 
-    frame = cv2.Canny(frame,100,10)
+    #
+    frame = cv2.applyColorMap(frame, cv2.COLORMAP_OCEAN)
 
     cv2.imshow('frame', frame)
 
