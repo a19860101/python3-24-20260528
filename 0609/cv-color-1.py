399a0996b3fae0f45dd6e51234df869b0b8f6490
Zac <a19860101@gmail.com>
2026-06-09 21:27:01 +0800

    20260609

--- 0609/cv-color-1.py
+++ 0609/cv-color-1.py
@@ -8,13 +8,15 @@ while True:
     hsv = cv2.cvtColor(frame, cv2.COLOR_BGR2HSV)
     # H:hue色相,S:saturation飽和度,V:value明度
     # BGR
-    lower = np.array([80,50,50])
-    upper = np.array([130,255,255])
+    lower = np.array([10,50,50])
+    upper = np.array([35,255,255])
 
     mask = cv2.inRange(hsv, lower, upper)
+    result = cv2.bitwise_and(frame, frame, mask=mask)
 
     cv2.imshow("original", frame)
     cv2.imshow("Mask", mask)
+    cv2.imshow("Result", result)
     if cv2.waitKey(1) == ord('q'):
         break
 
