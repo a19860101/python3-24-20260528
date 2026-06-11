6a16a768e5db8655e605de0c9f5cfbfb42fb7daa
Zac <a19860101@gmail.com>
2026-06-11 20:59:47 +0800

    20260611

--- 0611/yolo-1.py
+++ 0611/yolo-1.py
@@ -9,6 +9,9 @@ model = YOLO('yolov8n.pt')
 # results[0].show()
 
 
-model.predict(source='../video.mp4',show=True, conf=0.8)
-# model.predict(source=0, show=True)
+# model.predict(source='../video.mp4',show=True, conf=0.8)
+model.predict(source=0, show=True, conf=0.4,classes=[0,67,39])
 
+# print(model.names)
+for item in model.names.items():
+    print(item)
