3ec4da3e0374bfd9d3f6f85173350642c1d3a3d4
Zac <a19860101@gmail.com>
2026-06-16 19:00:53 +0800

    20260614

---
+++ 0616/yolo-face-3.py
@@ -0,0 +1,33 @@
+import cv2
+from ultralytics import YOLO
+
+model = YOLO('yolov8n.pt')
+
+cap = cv2.VideoCapture(0)
+
+if not cap.isOpened():
+    print('無法開啟攝影機........')
+    exit()
+
+while True:
+    ret, frame = cap.read()
+
+    if not ret:
+        break
+
+    result = model(frame, stream=True,conf=0.4,classes=[0])
+
+    person_detected = False
+    for r in result:
+        plot_frame = r.plot()
+        # print(f'box:{r.boxes}')
+        if len(r.boxes) > 0:
+            person_detected = True
+
+    cv2.imshow('YOLO v8', plot_frame)
+
+    if cv2.waitKey(1) & 0xFF == ord('q'):
+        break
+
+cap.release()
+cv2.destroyAllWindows()
