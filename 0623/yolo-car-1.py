cd0729bd3fe7f14d925474fe067a9b452e4e086f
Zac <a19860101@gmail.com>
2026-06-23 19:29:16 +0800

    20260623

--- 0623/yolo-car-1.py
+++ 0623/yolo-car-1.py
@@ -11,8 +11,10 @@ img = cv2.imread("../images/car-3.jpg")
 results = model(img)
 
 for result in results:
-    print(result.boxes)
-
-
+    # print(result.boxes)
+    for box in result.boxes:
+        # print(box.xyxy[0])
+        x1,y1,x2,y2 = map(int, box.xyxy[0])
+        print(x1,y1,x2,y2)
 cv2.imshow("Result", img)
 cv2.waitKey(0)
