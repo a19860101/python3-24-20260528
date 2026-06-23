5614af89e8852cc4af1a1c6fbdce7b8312adaf9d
Zac <a19860101@gmail.com>
2026-06-23 20:30:53 +0800

    20260623

--- 0623/yolo-car-1.py
+++ 0623/yolo-car-1.py
@@ -6,7 +6,9 @@ model = YOLO("best.pt")
 
 reader = easyocr.Reader(['en'], gpu=True)
 
-img = cv2.imread("../images/car-3.jpg")
+img = cv2.imread("../images/car-2.jpg")
+
+H, W, _ = img.shape
 
 results = model(img)
 
@@ -17,12 +19,19 @@ for result in results:
         x1,y1,x2,y2 = map(int, box.xyxy[0])
         print(x1,y1,x2,y2)
 
+        padding = 5
+        x1 = max(0,x1 - padding)
+        y1 = max(0,y1 - padding)
+        x2 = min(W,x2 + padding)
+        y2 = min(H,y2 + padding)
+
         plate = img[y1:y2,x1:x2]
+
         text = reader.readtext(plate)
         print(text)
 
         if text:
-            cv2.rectangle(img,(x1,y1),(x2,y2),(0,255,0),2)
+            cv2.rectangle(img,(x1,y1),(x2,y2),(0,255,0),1)
 
 
 cv2.imshow("Result", img)
