1ef52a25e75f12413404368727f4ea0567f44090
Zac <a19860101@gmail.com>
2026-07-02 19:11:21 +0800

    20260701

---
+++ 0702/ml-2.py
@@ -0,0 +1,45 @@
+from sklearn.tree import DecisionTreeClassifier
+from sklearn.model_selection import train_test_split
+
+X = [
+    [32,0],
+    [30,1],
+    [30,0],
+    [28,0],
+    [26,1],
+    [25,1],
+    [22,1],
+    [20,0],
+    [18,0],
+    [16,0]
+]
+y = [
+    '冰紅茶',
+    '珍珠奶茶',
+    '可樂',
+    '無糖綠茶',
+    '氣泡水',
+    '牛奶',
+    '水',
+    '冰美式',
+    '熱美式',
+    '熱拿鐵'
+]
+
+# 建立模型
+model = DecisionTreeClassifier()
+
+
+X_train, X_test, y_train, y_test = train_test_split(X, y, test_size = 0.2, random_state = 42)
+
+print(X_train)
+print(X_test)
+print(y_train)
+print(y_test)
+
+# 訓練
+model.fit(X,y)
+
+# 預測
+result = model.predict([[24,0]])
+print(result)
