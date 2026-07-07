fd772b2dcf29e379252624ac72750accc995360e
Zac <a19860101@gmail.com>
2026-07-07 19:35:05 +0800

    20260707

---
+++ 0707/regression-1.py
@@ -0,0 +1,30 @@
+from sklearn.linear_model import LinearRegression
+
+X = [
+    [10],
+    [20],
+    [30],
+    [40],
+    [50],
+    [60]
+]
+
+y = [101,212,280,382,472,589]
+
+model = LinearRegression()
+
+model.fit(X, y)
+
+m = model.coef_[0]
+b = model.intercept_
+
+print("線性回歸公式：")
+print(f"y = {m:.3f}x + {b:.3f}")
+
+print()
+
+x = int(input())
+
+result = m * x + b
+
+print(result)
