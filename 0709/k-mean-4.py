6aed1c5b05e38b57d03b310c350285c746fb389f
Zac <a19860101@gmail.com>
2026-07-09 21:22:25 +0800

    20260709

--- 0709/k-mean-4.py
+++ 0709/k-mean-4.py
@@ -1,27 +1,22 @@
 import matplotlib.pyplot as plt
 from sklearn.cluster import KMeans
 
-# 甜度、冰塊量
 X = [
-    [9, 8],
-    [8, 9],
-    [7, 8],
+    [0, 0],
+    [2, 0],
     [2, 2],
-    [3, 1],
-    [2, 3],
-    [5, 5],
-    [6, 4],
-    [9, 9],
-    [1, 2],
-    [6, 5],
-    [5, 4]
+    [4, 2],
+    [8, 7],
+    [8, 9],
+    [10, 7],
+    [10, 9],
 ]
 
 # 儲存不同 K 值的 inertia
 inertias = []
 
 # 測試 K = 1 到 K = 6
-for k in range(1, 7):
+for k in range(1, 6):
     model = KMeans(n_clusters=k, random_state=0)
     model.fit(X)
     inertias.append(model.inertia_)
@@ -29,9 +24,9 @@ for k in range(1, 7):
 print(inertias)
 
 # 畫出手肘圖
-plt.plot(range(1, 7), inertias, marker='o')
-
-plt.xlabel("K value")
-plt.ylabel("Inertia")
-plt.title("Elbow Method")
+# plt.plot(range(1, 7), inertias, marker='o')
+#
+# plt.xlabel("K value")
+# plt.ylabel("Inertia")
+# plt.title("Elbow Method")
 # plt.show()
